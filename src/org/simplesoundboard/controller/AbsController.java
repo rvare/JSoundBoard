0133ab4cb89fe2e418c2570d1365c16a273f0fa4
Richard Varela <richardvare13@gmail.com>
2026-01-27 10:49:57 -0800

    Created AbsController and Controller. However, there might be a lot of refactoring needed due to how things are getting implemented.

--- src/org/simplesoundboard/controller/AbsController.java
+++ src/org/simplesoundboard/controller/AbsController.java
@@ -1,3 +1,61 @@
-abstract public class AbsController {
+package org.simplesoundboard.controller;
 
+import java.util.*;
+import java.io.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import org.simplesoundboard.model.*;
+import org.simplesoundboard.view.*;
+
+abstract public class AbsController {
+	protected final Model model;
+	protected final IView iView;
+
+	public AbsController(final Model model, final IView iView) {
+		this.model = model;
+		this.iView = iView;
+	}
+
+	// Getters
+	public Model getModel() {
+		return this.model;
+	}
+
+	public IView getIView() {
+		return this.iView;
+	}
+
+	// Setters
+
+	// Operations
+	// Abstract methods
+	public void showAboutDialog();
+	public void showDocumentationDialog();
+
+	// Concrete methods
+		// File operations
+	public void loadPresetToModel(String filePresetPath) {
+		this.model.loadPreset(filePresetPath);
+	}
+
+	public void savePresetFromModel(String filePresetPath) {
+		this.model.savePreset(filePresetPath);
+	}
+
+		// Sound operations
+	public void addSoundToModel(String soundFilePath) {
+		this.model.subscribe(new SoundClip(soundFilePath, "");
+	}
+
+	public void removeSoundFromModel(SoundClip soundName) {
+		this.model.unsubscribe(soundName);
+	}
+
+	public void playSoundClip(String soundName) {
+		this.model.notifySubscribers(soundName);
+	}
 }
--- src/org/simplesoundboard/controller/Controller.java
+++ src/org/simplesoundboard/controller/Controller.java
@@ -1,5 +1,32 @@
 package org.simplesoundboard.controller;
 
-public class Controller {
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import javax.swing.*;
+import javax.swing.event.*;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
+import org.simplesoundboard.model.*;
+import org.simplesoundboard.view.*;
+import org.simplesoundboard.controller.AbsController;
+
+public class Controller extends AbsController {
+	public Controller(final Model model, final IView iView) {
+		super(model, iView);
+
+		// Create menu bar listeners
+
+		// Create button listeners
+
+	}
 }
