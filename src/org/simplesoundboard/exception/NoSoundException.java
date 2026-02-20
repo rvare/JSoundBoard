d45e5abfe76d1d18b6694938eb3d100e91468566
Richard Varela <richardvare13@gmail.com>
2026-02-20 15:08:53 -0800

    Added JavaDoc documentation to exception classes.

--- src/org/simplesoundboard/exception/NoSoundException.java
+++ src/org/simplesoundboard/exception/NoSoundException.java
@@ -1,6 +1,14 @@
 package org.simplesoundboard.exception;
 
+/**
+ * Thrown when there is no sound.
+ * @author Richard Varela
+ */
 public class NoSoundException extends Exception {
+	/**
+	 * Default constuctor.
+	 * @param errorMessage A String object that represents the error message to be displayed.
+	 */
 	public NoSoundException(String errorMessage) {
 		super(errorMessage);
 	}
--- src/org/simplesoundboard/exception/SoundNameConflictException.java
+++ src/org/simplesoundboard/exception/SoundNameConflictException.java
@@ -1,6 +1,14 @@
 package org.simplesoundboard.exception;
 
+/**
+ * Thrown when user attempts to reuse a sound name more than once.
+ * @author Richard Varela
+ */
 public class SoundNameConflictException extends Exception {
+	/**
+	 * Default constuctor.
+	 * @param errorMessage A String object that represents the error message to be displayed.
+	 */
 	public SoundNameConflictException(String errorMessage) {
 		super(errorMessage);
 	}
